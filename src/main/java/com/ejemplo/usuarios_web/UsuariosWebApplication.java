8d94a677099adbcc1b86b3155c26369be926151a
Karlx28 <mo0436032023@unab.edu.sv>
2025-04-30 12:17:24 -0600

    Empezando proyecto

---
+++ src/main/java/com/ejemplo/usuarios_web/UsuariosWebApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.usuarios_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejemplo/usuarios_web/UsuariosWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.usuarios_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
